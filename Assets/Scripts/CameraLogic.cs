7d912d0fa31c109784d0a95393714a47f4c84123
Andrey Viktorov <andrey.viktorov@gmail.com>
2015-08-25 02:01:43 +0600

    Fix camera flythroughs

---
+++ Assets/Scripts/CameraLogic.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class CameraLogic : MonoBehaviour {
+	
+	private Animation cachedAnimation;
+	private bool paused;
+	
+	private void Awake() {
+		cachedAnimation = GetComponent<Animation>();
+		paused = false;
+	}
+	
+	private void Update() {
+		if (!paused) return;
+		if (Input.GetKeyDown(KeyCode.W)) Next();
+		if (Input.GetKeyDown(KeyCode.S)) Prev();
+	}
+	
+	private void Next() {
+		foreach (AnimationState state in cachedAnimation) {
+			state.speed = 1.0f;
+		}
+		paused = false;
+	}
+	
+	private void Prev() {
+		foreach (AnimationState state in cachedAnimation) {
+			state.speed = -1.0f;
+		}
+		paused = false;
+	}
+	
+	public void Pause () {
+		foreach (AnimationState state in cachedAnimation) {
+			state.speed = 0.0f;
+		}
+		paused = true;
+	}
+}

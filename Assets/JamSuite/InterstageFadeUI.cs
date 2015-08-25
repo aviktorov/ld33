ac28015140b262c6d509ef3fb546afb3852f7d76
Anton Tarasov <uchiro.me@gmail.com>
2015-08-25 20:19:35 +0600

    Rewrite interstage fade ui

--- Assets/JamSuite/InterstageFadeUI.cs
+++ Assets/JamSuite/InterstageFadeUI.cs
@@ -2,35 +2,28 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 
-[RequireComponent(typeof(Image))]
 public class InterstageFadeUI : MonoBehaviour {
 
 	public AnimationCurve inTransition = AnimationCurve.EaseInOut(0f, 0f, 0.5f, 1f);
 	public AnimationCurve outTransition = AnimationCurve.EaseInOut(0f, 1f, 0.5f, 0f);
 
 	public void FadeToLevel(string level) {
-		StartCoroutine(DoFadeOut(level));
+		GetComponentInChildren<Image>().enabled = true;
+		StartCoroutine(DoFade(level));
 	}
 
-	private void Start() {
-		GetComponent<Image>().enabled = true;
-		StartCoroutine(DoFadeIn());
-	}
-
-	private IEnumerator DoFadeIn() {
-		yield return StartCoroutine(DoAnimate(outTransition));
-
-		gameObject.SetActive(false);
-	}
-
-	private IEnumerator DoFadeOut(string level) {
+	private IEnumerator DoFade(string level) {
+		DontDestroyOnLoad(gameObject);
 		yield return StartCoroutine(DoAnimate(inTransition));
 
 		Application.LoadLevel(level);
+		yield return StartCoroutine(DoAnimate(outTransition));
+
+		Destroy(gameObject);
 	}
 
 	private IEnumerator DoAnimate(AnimationCurve curve) {
-		var image = GetComponent<Image>();
+		var image = GetComponentInChildren<Image>();
 		var duration = curve[curve.length - 1].time;
 
 		for (var t = 0f; t <= duration; t += Mathf.Max(0.001f, Time.deltaTime)) {

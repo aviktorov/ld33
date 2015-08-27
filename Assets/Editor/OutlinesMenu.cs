277ead6bc110d7a5dca7174890226b2664fc537a
Anton Tarasov <uchiro.me@gmail.com>
2015-08-27 14:03:05 +0600

    Move creating outline models to the editor

---
+++ Assets/Editor/OutlinesMenu.cs
@@ -0,0 +1,65 @@
+﻿#if UNITY_EDITOR
+
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public static class OutlinesMenu{
+	[MenuItem("Impostor/Create Outlines")]
+	public static void CreateOutlines() {
+		GameObject[] itemObjects = GameObject.FindGameObjectsWithTag("Item");
+
+		foreach (var itemObject in itemObjects) {
+			Transform cachedTransform = itemObject.GetComponent<Transform>();
+			Mesh mesh = itemObject.GetComponent<MeshFilter>().mesh;
+
+			GameObject outline = new GameObject("Outlines");
+			outline.transform.SetParent(cachedTransform, false);
+
+			outline.AddComponent<MeshRenderer>();
+			Mesh outlineMesh = outline.AddComponent<MeshFilter>().mesh;
+
+			Vector3[] vertices = mesh.vertices;
+			Vector3[] normals = mesh.normals;
+			Vector2[] uv = mesh.uv;
+			int[] triangles  = mesh.triangles ;
+
+			Vector3[] outlineVertices = new Vector3[vertices.Length];
+			Vector2[] outlineUV = new Vector2[uv.Length];
+			Vector3[] outlineNormals = new Vector3[normals.Length];
+			int[] outlineTriangles  = new int[triangles.Length];
+			
+			float maxScale = Mathf.Max(Mathf.Abs(cachedTransform.localScale.x), Mathf.Abs(cachedTransform.localScale.y), Mathf.Abs(cachedTransform.localScale.z));
+			for (int i = 0; i < outlineVertices.Length; i++) 
+				outlineVertices[i] = vertices[i] + normals[i] * OutlineSettings.instance.width / maxScale;
+
+			for (int i = 0; i < outlineUV.Length; i++)
+				outlineUV[i] = uv[i];
+
+			// Flip normals
+			for (int i = 0; i < outlineNormals.Length; i++)
+				outlineNormals[i] = -normals[i];
+
+			for (int i = 0; i < outlineTriangles.Length; i += 3) {
+				outlineTriangles[i + 1] = triangles[i];
+				outlineTriangles[i] = triangles[i + 1];
+				outlineTriangles[i + 2] = triangles[i + 2];
+			}
+
+			outlineMesh.vertices = outlineVertices;
+			outlineMesh.uv = outlineUV;
+			outlineMesh.triangles  = outlineTriangles;
+			outlineMesh.normals = outlineNormals;
+
+			outline.SetActive(false);
+
+			Item item = itemObject.GetComponent<Item>();
+			if (item != null) {
+				item.outline = outline;
+				item.outlineRenderer = outline.GetComponent<Renderer>();
+			}
+		}
+	}
+}
+
+#endif
--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -11,46 +11,46 @@ public class Item : MonoBehaviour {
 	public string price = "100₽";
 	public string description = "• Wow! This is brand new TV! And they added remote control to it.";
 
-	private Transform cachedTransform;
-
-	private GameObject outline;
-	private Renderer cachedOutlineRenderer;
+	[HideInInspector]
+	public GameObject outline;
+	
+	[HideInInspector]
+	public Renderer outlineRenderer;
 
 	private bool selectedDebug = false;
 
 	private void Awake() {
-		cachedTransform = GetComponent<Transform>();
-
-		CreateOutline();
+		outlineRenderer.sharedMaterial = OutlineSettings.instance.highlightedMaterial;
+		outlineRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
 	}
 
 	public void Select() {
 		selected = true;
 		if (!selectedDebug)
-			cachedOutlineRenderer.sharedMaterial = OutlineSettings.instance.selectedMaterial;
+			outlineRenderer.sharedMaterial = OutlineSettings.instance.selectedMaterial;
 	}
 
 	public void Unselect() {
 		selected = false;
 		if (!selectedDebug)
-			cachedOutlineRenderer.sharedMaterial = OutlineSettings.instance.highlightedMaterial;
+			outlineRenderer.sharedMaterial = OutlineSettings.instance.highlightedMaterial;
 	}
 
 	public void SelectedDebug() {
 		if (selectedDebug) {
 			selectedDebug = false;
 			if (selected) {
-				cachedOutlineRenderer.sharedMaterial = OutlineSettings.instance.selectedMaterial;
+				outlineRenderer.sharedMaterial = OutlineSettings.instance.selectedMaterial;
 			}
 			else {
 				outline.SetActive(false);
-				cachedOutlineRenderer.sharedMaterial = OutlineSettings.instance.highlightedMaterial;
+				outlineRenderer.sharedMaterial = OutlineSettings.instance.highlightedMaterial;
 			}
 		}
 		else {
 			outline.SetActive(true);
 			selectedDebug = true;
-			cachedOutlineRenderer.sharedMaterial = OutlineSettings.instance.selectedDebugMaterial;
+			outlineRenderer.sharedMaterial = OutlineSettings.instance.selectedDebugMaterial;
 		}
 	}
 
@@ -61,53 +61,5 @@ public class Item : MonoBehaviour {
 	public void Hide() {
 		if (!selected && !selectedDebug)
 			outline.SetActive(false);
-	}
-
-	private void CreateOutline() {
-		Mesh mesh = GetComponent<MeshFilter>().mesh;
-
-		outline = new GameObject("Outliner");
-		outline.transform.SetParent(cachedTransform, false);
-
-		outline.AddComponent<MeshRenderer>();
-		Mesh outlineMesh = outline.AddComponent<MeshFilter>().mesh;
-
-		Vector3[] vertices = mesh.vertices;
-		Vector3[] normals = mesh.normals;
-		Vector2[] uv = mesh.uv;
-		int[] triangles  = mesh.triangles ;
-
-		Vector3[] outlineVertices = new Vector3[vertices.Length];
-		Vector2[] outlineUV = new Vector2[uv.Length];
-		Vector3[] outlineNormals = new Vector3[normals.Length];
-		int[] outlineTriangles  = new int[triangles.Length];
-		
-		float maxScale = Mathf.Max(Mathf.Abs(cachedTransform.localScale.x), Mathf.Abs(cachedTransform.localScale.y), Mathf.Abs(cachedTransform.localScale.z));
-		for (int i = 0; i < outlineVertices.Length; i++) 
-			outlineVertices[i] = vertices[i] + normals[i] * OutlineSettings.instance.width / maxScale;
-
-		for (int i = 0; i < outlineUV.Length; i++)
-			outlineUV[i] = uv[i];
-
-		// Flip normals
-		for (int i = 0; i < outlineNormals.Length; i++)
-			outlineNormals[i] = -normals[i];
-
-		for (int i = 0; i < outlineTriangles.Length; i += 3) {
-			outlineTriangles[i + 1] = triangles[i];
-			outlineTriangles[i] = triangles[i + 1];
-			outlineTriangles[i + 2] = triangles[i + 2];
-		}
-
-		outlineMesh.vertices = outlineVertices;
-		outlineMesh.uv = outlineUV;
-		outlineMesh.triangles  = outlineTriangles;
-		outlineMesh.normals = outlineNormals;
-
-		cachedOutlineRenderer = outline.GetComponent<Renderer>();
-		cachedOutlineRenderer.sharedMaterial = OutlineSettings.instance.highlightedMaterial;
-		cachedOutlineRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
-
-		outline.SetActive(false);
 	}
 }

e0af60f059613c1a96f08d3cc3bfd22cd24198d5
Anton Tarasov <uchiro.me@gmail.com>
2017-02-09 14:20:24 +0700

    Refactor GameLogic

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -18,40 +18,34 @@ public class GameLogic : MonoSingleton<GameLogic> {
 		db = GlobalSettings.instance.db;
 
 		GameObject[] itemObjects = GameObject.FindGameObjectsWithTag("Item");
-		Item[] items = new Item[itemObjects.Length];
-		for (int i = 0; i < items.Length; ++i) {
-			items[i] = itemObjects[i].GetComponent<Item>();
-			if (items[i] != null)
-				items[i].type = db.types[items[i].index];
+			List<Item> items = new List<Item>();
+		for (int i = 0; i < itemObjects.Length; ++i) {
+			Item item = itemObjects[i].GetComponent<Item>();
+			if (item != null) {
+				items.Add(item);
+				item.type = db.types[item.index];
+			}
 		}
 
-		int mimicNumber = 0;
-		bool chose = false;
-		while (!chose) {
-			mimicNumber = Random.Range(0, items.Length);
-			if (items[mimicNumber] == null) continue;
-
-			chose = true;
+		// Set mimic
+		bool rightChoise = false;
+		while (!rightChoise) {
+			int mimicNumber = Random.Range(0, items.Count);
+			rightChoise = true;
 			for (int i = 1; i <= 7; i++) {
 				if (items[mimicNumber].type == ("Note" + i)) {
-					chose = false;
+					rightChoise = false;
 					break;
 				}
+			}
+			if (rightChoise) {
+				mimic = items[mimicNumber];
+				items[mimicNumber].mimic = true;
 			}
 		}
 
 		theme = db.themes[Random.Range(0, db.themes.Count)];
-		int currentNumber = 0;
 		foreach (var item in items) {
-			if (item == null) continue;
-			item.type = db.types[item.index];
-
-			// Set mimic
-			if (mimicNumber == currentNumber) {
-				mimic = item;
-				item.mimic = true;
-			}
-			
 			// Set name and price
 			List<NameData> names = (from name in db.names where name.type == item.type select name).ToList();
 			int numNames = Random.Range(0, names.Count);
@@ -61,47 +55,45 @@ public class GameLogic : MonoSingleton<GameLogic> {
 			// Set descriptions
 			List<DescriptionData> descriptions = new List<DescriptionData>();
 			foreach (var d in db.descriptions) {
-				if(item.type != d.type) continue;
-				if(d.mimic && mimicNumber != currentNumber) continue;
-				if(d.theme != "" && d.theme != theme) continue;
-				
+				if (item.type != d.type) continue;
+				if (d.mimic && item != mimic) continue;
+				if (d.theme != "" && d.theme != theme) continue;
+
 				descriptions.Add(d);
 			}
-			
+
 			if (descriptions.Count == 0) continue;
-			
+
 			List<DescriptionData> chosenDescriptions = new List<DescriptionData>();
 			int groupsNumber = descriptions.Max(d => d.group);
 			for (int i = 0; i <= groupsNumber; i++) {
 				List<DescriptionData> descriptionsInGroup = new List<DescriptionData>();
 				foreach (var d in descriptions)
-					if (d.group == i) 
+					if (d.group == i)
 						descriptionsInGroup.Add(d);
 				if (descriptionsInGroup.Count > 0) {
 					chosenDescriptions.Add(descriptionsInGroup[Random.Range(0, descriptionsInGroup.Count)]);
 				}
 			}
-			
-			int descriptionsNumber = 0;
+
 			item.description = "";
 			for (int i = 0; i < chosenDescriptions.Count; i++) {
 				if (chosenDescriptions[i].probability >= Random.Range(0.0f, 1.0f)) {
-					descriptionsNumber++;
 					item.description += "• " + chosenDescriptions[i].text + "\n";
 				}
 			}
-			if (descriptionsNumber == 0)
+			if (string.IsNullOrEmpty(item.description))
 				item.description += "• " + chosenDescriptions[Random.Range(0, chosenDescriptions.Count)].text + "\n";
-
-			currentNumber++;
 		}
 	}
 
-	public void Update() {			
+	public void Update() {
+		#if UNITY_EDITOR
 		if (Input.GetKeyDown(KeyCode.Space) && mimic != null) {
 			Debug.Log(mimic.gameObject.name);
 			mimic.SelectedDebug();
 		}
+		#endif
 	}
 
 	public void EndGame() {
